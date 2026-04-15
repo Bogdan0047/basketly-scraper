33c58b5a0d886a790675ee81f30d3b79692fcdef
Bogdan0047 <simion.bogdan1991@gmail.com>
2026-04-15 10:20:39 +0100

    Update scraper import to use sainsburys module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { scrape } = require("./scraper");
+const sainsburys = require("./scrapers/sainsburys");
 
 const app = express();
 app.use(express.json());
