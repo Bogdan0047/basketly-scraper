2172cce7e04376075dde8bb9ecfb01f53a22b577
Bogdan0047 <simion.bogdan1991@gmail.com>
2026-04-15 10:08:44 +0100

    Refactor scrape function to use switch statement

--- src/scraper.js
+++ src/scraper.js
@@ -3,14 +3,14 @@ const sainsburys = require("./scrapers/sainsburys");
 async function scrape(retailer, query) {
   console.log("SCRAPE CALLED:", retailer, query);
 
-  if (retailer === "sainsburys") {
-    return await sainsburys.search(query);
-  }
+  switch (retailer) {
+    case "sainsburys":
+      return await sainsburys.search(query);
 
-  return [];
+    default:
+      console.log("Unknown retailer:", retailer);
+      return [];
+  }
 }
 
 module.exports = { scrape };
-
-console.log("SCRAPE CALLED:", retailer, query);
-console.log("USING SAINSBURYS ROUTE");
